b4a836d52e14c5f303ecea505f62cce856f6c0fb
张贵林 <zhangguilin@baklib.vip>
2023-08-09 22:33:51 +0800

    scaffold to artist and record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_102056) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_105122) do
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_artists_on_user_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string "title"
+    t.string "year"
+    t.integer "artist_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_records_on_artist_id"
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -18,4 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_102056) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "artists", "users"
+  add_foreign_key "records", "artists"
+  add_foreign_key "records", "users"
 end
